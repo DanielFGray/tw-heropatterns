41fb045f99409547559d46e86083d45abad87b9c
Daniel Gray <DanielFGray@gmail.com>
2021-04-20 18:55:30 -0500

    making things tidier

--- tailwind-heropatterns.js
+++ tailwind-heropatterns.js
@@ -2,18 +2,18 @@ const plugin = require('tailwindcss/plugin')
 const {nameClass} = require('tailwindcss/jit/pluginUtils')
 const patterns = require('./heropatterns')
 
-module.exports = function HeroPatterns({ matchUtilities, theme }) {
+module.exports = plugin(function HeroPatterns({ matchUtilities, theme }) {
   matchUtilities({
-    bgp: (modifier) => {
+    bg: (modifier) => {
       const [name, opacity, ...colorParts] = modifier.split('-')
-      const color = theme(`colors.${colorParts.join('.')}`)
+      const color = theme(['colors', ...colorParts])
       if (!patterns[name] || !color || !opacity) return []
-      const pattern = patterns[name]({ opacity, color: color.replace('#', '%23') })
+      const pattern = patterns[name]({ opacity, color })
       return {
-        [nameClass('bgp', modifier)]: {
-          backgroundImage: `url('data:image/svg+xml,${pattern}')`
+        [nameClass('bg', modifier)]: {
+          backgroundImage: `url('data:image/svg+xml,${encodeURIComponent(pattern)}')`
         }
       }
     }
   })
-}
+})
