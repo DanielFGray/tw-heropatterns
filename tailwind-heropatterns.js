321c4226229c626aa341c417bccba85ca0052eaa
Daniel Gray <DanielFGray@gmail.com>
2021-04-22 13:15:54 -0500

    revert accidental bgp -> bg prefix mismatch

--- tailwind-heropatterns.js
+++ tailwind-heropatterns.js
@@ -4,13 +4,13 @@ const patterns = require('./heropatterns')
 
 module.exports = plugin(function HeroPatterns({ matchUtilities, theme }) {
   matchUtilities({
-    bg: (modifier) => {
+    bgp: (modifier) => {
       const [name, opacity, ...colorParts] = modifier.split('-')
       const color = theme(['colors', ...colorParts])
       if (!patterns[name] || !color || !opacity) return []
       const pattern = patterns[name]({ opacity, color })
       return {
-        [nameClass('bg', modifier)]: {
+        [nameClass('bgp', modifier)]: {
           backgroundImage: `url('data:image/svg+xml,${encodeURIComponent(pattern)}')`
         }
       }
